45f98fdedb2d5fe99ee6e35263f47476acec632e
Josh Lefebvre <josh.lefebvre@lefewaresolutions.com>
2020-08-19 08:40:35 -0400

    Working on AKS arm templats

--- AzureCLI/AKS/Helm/HelmAKSCLI.ps1
+++ AzureCLI/AKS/Helm/HelmAKSCLI.ps1
@@ -1,7 +1,7 @@
-$location="eastus"
-$rgName="Azure303"
-$clusterName="LWSMyAKSCluster"
-$acrName="LWSMyHelmACR"
+location="eastus"
+rgName="Azure303"
+clusterName="LWSMyAKSCluster"
+acrName="LWSMyHelmACR"
 
 az login 
 
@@ -10,3 +10,11 @@ az acr create --resource-group $rgName --name $acrName --sku Basic
 
 #Configures the appropriate ACRPull role for the service principal
 az aks create -g $rgName -n $clusterName --location $location  --attach-acr $acrName --generate-ssh-keys
+
+#Build and push webfrontend project to ACR
+az acr build --image webfrontend:v1 --registry $acrName --file Dockerfile .
+
+helm create webfrontend
+
+
+az group delete --name $rgName --yes --no-wait
