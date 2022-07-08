ec6cd82eaa8b45e2dd4a94ab964d912b7d7a1941
josh.lefebvre@lefewaresolutions.com <josh.lefebvre@lefewaresolutions.com>
2022-07-07 20:00:34 -0400

    Add Container instances

--- PowerShell/ContainerInstance/CreateContainerInstance.ps1
+++ PowerShell/ContainerInstance/CreateContainerInstance.ps1
@@ -1,16 +1,29 @@
 $location = "eastus"
-$resourceGroupName = "Azure303"
+$resourceGroupName = "Azure104"
 $containerInstanceName = "mycontainer"
 
 
 Connect-AzAccount
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
-New-AzContainerGroup -ResourceGroupName $resourceGroupName `
-  -Name $containerInstanceName `
-  -Image mcr.microsoft.com/azuredocs/aci-helloworld `
+#Create a container group with a container instance and request a public IP address with opening ports
+$port1 = New-AzContainerInstancePortObject -Port 8000 -Protocol TCP
+$port2 = New-AzContainerInstancePortObject -Port 8001 -Protocol TCP
+
+$container = New-AzContainerInstanceObject -Name test-container `
+  -Image nginx `
+  -RequestCpu 1 `
+  -RequestMemoryInGb 1.5 `
+  -Port @($port1, $port2)
+
+$containerGroup = New-AzContainerGroup `
+  -ResourceGroupName $resourceGroupName `
+  -Name $containerInstanceNAme `
+  -Location eastus `
+  -Container $container `
   -OsType Linux `
-  -DnsNameLabel lws-aci-demo-win `
-  -location "westus"
+  -RestartPolicy "Never" `
+  -IpAddressType Public
+
 
 Get-AzContainerGroup -ResourceGroupName $resourceGroupName -Name $containerInstanceName
